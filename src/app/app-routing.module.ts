904d6303f98b5601b6e31a015302bffbabb8cc93
Ernest <perkowski.ernest98@gmail.com>
2020-12-02 22:25:14 +0100

    navbar: styling, remove contact page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutMeComponent } from './components/about/about.component';
-import { ContactMeComponent } from './components/contact/contact.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 const routes: Routes = [
   { path: '', component: AboutMeComponent },
-  { path: 'contact', component: ContactMeComponent },
   { path: 'portfolio', component: PortfolioComponent },
   { path: '', redirectTo: '', pathMatch: 'full' },
 ];
